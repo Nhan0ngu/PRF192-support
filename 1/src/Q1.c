a278ca8cb6a39bc9ed651e83b0d49bd99394b9ef
nhan <nhanltse172769@fpt.edu.vn>
2022-07-15 23:16:10 +0700

    LOZ

--- 1/src/Q1.c
+++ 1/src/Q1.c
@@ -11,5 +11,18 @@ double average(int a, int b, int c){
 int main()
 {
   //=============DO NOT ADD NEW OR CHANGE THIS STATEMENTS========
+  system("cls");
+  printf("\nTEST Q1 (2 marks):\n");
+  int a, b, c;
+  double r;
+  printf("Enter a = "); scanf("%d",&a);
+  printf("Enter b = "); scanf("%d",&b);
+  printf("Enter c = "); scanf("%d",&c);   
+  r = average(a,b,c); 
+  printf("\nOUTPUT:\n");
+  printf("%.2lf",r); 
+  printf("\n");
+  system ("pause");
+  return(0);
   //============================================================ 
 }
